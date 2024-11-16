a6bbd161763bd99050ff64b354a7ad06de8224a8
Philipp Mukosey <philipp.mukosey@tha.de>
2024-11-16 12:17:15 +0100

    Add more message types

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.responses import HTMLResponse
 
 from uuid import uuid4, UUID
 
+import json
+
 app = FastAPI()
 
 html = """
@@ -41,15 +43,26 @@ html = """
 
 
 class User:
-    def __init__(self, ws: WebSocket = None, nickname: str = None, admin: UUID = None):
-        self.nickname =  nickname
+    def __init__(self, ws: WebSocket = None, username: str = None, admin: bool = False):
         self.ws = ws
+        self.username = username
         self.admin = admin
+        self.group_id = None
+        self.image = None
+
+
+class Group:
+    def __init__(self, admin_id: UUID, name: str = None):
+        self.name = name
+        self.admin_id = admin_id  # admin user id
+        self.image = None
+        self.participants = list()
 
 
 class UserManager:
     def __init__(self):
         self.__users = dict()
+        self.__groups = dict()
 
     async def connect(self, ws: WebSocket) -> UUID:
         id = uuid4()
@@ -70,9 +83,40 @@ class UserManager:
             return
         await ws.send_text(message)
 
-    async def change_nickname(self, id: UUID, new_nickname: str):
-        self.__users[id].nickname = new_nickname
-        await self.notify_group(f'nickname_changed|{id}|{new_nickname}')
+
+    async def set_user_name(self, id: UUID, new_username: str):
+        self.__users[id].username = new_username
+        await self.send('set_user_name|ok')
+
+    async def get_user_name(self, id: UUID):
+        await self.send(f'get_user_name|{self.__users[id].username}')
+
+
+    async def set_user_image(self, id: UUID, image: str):
+        self.__users[id].image = image
+        await self.send('set_user_image|ok')
+
+    async def get_user_image(self, id: UUID):
+        await self.send(f'get_user_image|{self.__users[id].image}')
+
+    
+    async def create_group(self, user_id: UUID):
+        user = self.__users[id]
+        if user.group_id is not None:
+            old_group = self.__groups[user.group_id]
+            if old_group.admin_id == user_id:
+                await self.send(f'create_group|error|this user already is an admin in another group')
+                return
+            await self.send(f'create_group|error|this user is already a member of another group')
+            return
+        new_group_id = uuid4()
+        self.__groups[new_group_id] = Group(admin_id=user_id)
+        self.__users[user_id].group_id = new_group_id
+        await self.send(f'create_group|{new_group_id}')
+
+    # async def get_group(self, user_id)
+
+
 
     def set_admin(self, id: UUID, admin: UUID):
         self.__users[id].admin = admin
@@ -121,25 +165,70 @@ manager = UserManager()
 async def get():
     return HTMLResponse(html)
 
-
+ 
 @app.websocket("/ws")
 async def websocket_endpoint(ws: WebSocket):
+    # отдаём объекты или данные (ник / аватар / ... отдельно)?
+    # database?
+    # размер картинок
+    # frontend получает сломанную картинку
+    
     await ws.accept()
     id = await manager.connect(ws)
     try:
         while True:
-            action, *payload = (await ws.receive_text()).split('|')
+            action, *payload = (await ws.receive_text()).split('|', 1)
             await ws.send_text(f"Message text was: {action}")
 
             match action:
-                case 'change_nickname':
-                    await manager.change_nickname(id, payload[0])
+                case 'get_user_image':
+                    await manager.get_image(id)
+                case 'set_user_image':
+                    await manager.set_image(id, payload[0])
+
+                case 'get_user_name':
+                    await manager.get_user_name(id)
+                case 'set_user_name':
+                    await manager.set_user_name(id, payload[0])
+
+                case 'get_group_name': # id в запросе
+                    # await manager.set_user_name(id, payload[0])
+                case 'set_group_name': # id в запросе
+                    # await manager.set_user_name(id, payload[0])
+
+                case 'get_group_image': # id в запросе
+                    # await manager.set_image(id, payload[0])
+                case 'set_group_image': # id в запросе
+                    await manager.set_image(id, payload[0])
+
+                case 'get_groups':
+                    # await manager.set_user_name(id, payload[0])
+                    # <- get_groups|group_id,group_id,group_id
+
+                case 'get_group_members': # group id в запросе
+                    # await manager.set_user_name(id, payload[0])
+                    # <- get_group_members|user_id,user_id,user_id
+
+                case 'add_group_member': # group id в запросе
+                    # await manager.set_image(id, payload[0])
+
+                case 'set_user_ready': # true / false в запросе
+                    # await manager.set_image(id, payload[0])
+
+                
+
+
+
+
+                case 'restore_user_id':
+                    await manager.restore(id, UUID(payload[0]), ws)
+
+
                 case 'broadcast':
                     await manager.broadcast('|'.join(payload))
-                case 'restore_id':
-                    await manager.restore(id, UUID(payload[0]), ws)
                 case 'clients':
                     await ws.send_text(f'clients are {manager.get_ids()}')
+
                 case _:
                     await ws.send_text('Unknown request')
     except WebSocketDisconnect:
